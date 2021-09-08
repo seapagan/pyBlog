52525874fa9cfbb5130703da57f0cee35a8f01fd
Grant Ramsay <seapagan@gmail.com>
2021-09-08 12:18:54 +0100

    Add auto slug and use in links
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/admin.py
+++ blog/admin.py
@@ -8,9 +8,10 @@ class BlogAdmin(admin.ModelAdmin):
     """Define a custom admin class for the Blog model."""
 
     date_hierarchy = "created_at"
-    list_display = ["title", "created_at", "updated_at"]
+    list_display = ["title", "created_at", "updated_at", "slug"]
     ordering = ("-created_at",)
     search_fields = ["title"]
+    readonly_fields = ["slug"]
 
 
 admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 """Define the Database models for this application."""
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Blog(models.Model):
@@ -13,7 +14,13 @@ class Blog(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     body = models.TextField()
+    slug = models.SlugField(default="")
 
     def __str__(self):
         """Return string representation of the Blog object."""
         return self.title
+
+    def save(self, *args, **kwargs):
+        """Override the save fumction, so we can generate the slug."""
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)

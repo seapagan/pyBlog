6262761f8cae18b1fd15f336dc76ecd4fdf65cc4
Grant Ramsay <seapagan@gmail.com>
2021-09-08 14:49:13 +0100

    Change index to TemplateView
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 """Define the views for the 'blog' application."""
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 
 from blog.models import Blog
 
 
-class IndexClassView(ListView):
-    """Define a ListView for the index (Blog main page)."""
+class IndexClassView(TemplateView):
+    """Define a TemplateView for the index (Blog main page)."""
 
-    model = Blog
     template_name = "blog/index.html"
-    context_object_name = "blogs"
+
+    def get_context_data(self, **kwargs):
+        """Return the context for this view.
+
+        Doing it this way so as to enable multiple models to be shown in the
+        template.
+        """
+        context = super(IndexClassView, self).get_context_data(**kwargs)
+        context["blogs"] = Blog.objects.all()
+        return context

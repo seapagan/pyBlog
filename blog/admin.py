c17bd978f6fd80a2b280beae605ec617764ede49
Grant Ramsay <seapagan@gmail.com>
2021-09-08 10:28:28 +0100

    Added Blog model and admin customisation
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
+"""Configure and customise the default Admin site."""
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+class BlogAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    list_display = ["title", "created_at", "updated_at"]
+    ordering = ("-created_at",)
+    search_fields = ["title"]
+
+
+admin.site.register(Blog, BlogAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+"""Define the Database models for this application."""
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    """Define the blog model.
+
+    This will contain individual blog entries.
+    """
+
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title

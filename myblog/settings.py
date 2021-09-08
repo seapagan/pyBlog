ff6e4ec45764d286eb5f21dab624d95a4a2b7b8d
Grant Ramsay <seapagan@gmail.com>
2021-09-08 17:03:06 +0100

    add handling for uploaded media
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- myblog/settings.py
+++ myblog/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "static"
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,10 @@
 """Root URL Configuration."""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

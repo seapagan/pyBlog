1cc10697fe7d71d4854951b22c3836621c931bc4
Grant Ramsay <seapagan@gmail.com>
2021-09-09 12:05:39 +0100

    Add basic detail view and url by slug
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 from blog import views
 
 app_name = "blog"
-urlpatterns = [path("", views.IndexClassView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.IndexClassView.as_view(), name="index"),
+    path("<str:slug>", views.PostDetailView.as_view(), name="detail"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 """Define the views for the 'blog' application."""
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, TemplateView
 
 from blog.models import Blog
 
@@ -18,3 +18,10 @@ class IndexClassView(TemplateView):
         context = super(IndexClassView, self).get_context_data(**kwargs)
         context["blogs"] = Blog.objects.all()
         return context
+
+
+class PostDetailView(DetailView):
+    """Display an actual blog post."""
+
+    model = Blog
+    template = "blog/detail.html"

e0191f2cacffd5ee4f705d38badce36543ff7305
Grant Ramsay <seapagan@gmail.com>
2021-09-22 11:23:40 +0100

    Add basic index pagination
    Also convert index back to a ListView
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,33 @@
 """Define the views for the 'blog' application."""
-from django.views.generic import DetailView, TemplateView
+from django.views.generic import DetailView, ListView
 
 from blog.models import Blog
 
 
-class IndexClassView(TemplateView):
+class IndexClassView(ListView):
     """Define a TemplateView for the index (Blog main page)."""
 
     template_name = "blog/index.html"
+    context_object_name = "blogs"
+    paginate_by = 6
+    model = Blog
 
-    def get_context_data(self, **kwargs):
-        """Return the context for this view.
 
-        Doing it this way (and not using a ListView so as to enable multiple
-        models to be shown in the template.
-        """
-        context = super(IndexClassView, self).get_context_data(**kwargs)
-        context["blogs"] = Blog.objects.all()
-        return context
+# class IndexClassView(TemplateView):
+#     """Define a TemplateView for the index (Blog main page)."""
+
+#     template_name = "blog/index.html"
+#     paginate_by = 4
+
+#     def get_context_data(self, **kwargs):
+#         """Return the context for this view.
+
+#         Doing it this way (and not using a ListView so as to enable multiple
+#         models to be shown in the template.
+#         """
+#         context = super(IndexClassView, self).get_context_data(**kwargs)
+#         context["blogs"] = Blog.objects.all()
+#         return context
 
 
 class PostDetailView(DetailView):

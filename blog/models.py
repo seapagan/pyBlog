5eb5bda7b393efdd11e602b5b6e94662b6977ee1
Grant Ramsay <seapagan@gmail.com>
2021-09-22 16:47:47 +0100

    add 'User' to the Blog model as Fgn Key
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 """Define the Database models for this application."""
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.urls import reverse
 from preferences.models import Preferences
 
 
@@ -10,6 +12,7 @@ class Blog(models.Model):
     This will contain individual blog entries.
     """
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=50)
     desc = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -30,6 +33,9 @@ class Blog(models.Model):
         """Override the save fumction, so we can generate the slug."""
         self.slug = slugify(self.title)
         super(Blog, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("blog:detail", args=[self.slug])
 
 
 class SitePreferences(Preferences):

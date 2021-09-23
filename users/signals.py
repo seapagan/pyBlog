0913e3fe64f257a62abdba2bee43f74374c76dc8
Grant Ramsay <seapagan@gmail.com>
2021-09-23 08:48:22 +0100

    auto create profile on new user
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

---
+++ users/signals.py
@@ -0,0 +1,19 @@
+"""Signals for the User App."""
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from users.models import Profile
+
+
+@receiver(post_save, sender=User)
+def build_profile(sender, instance, created, **kwargs):
+    """Build the profile object."""
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    """Save the profile."""
+    instance.profile.save()

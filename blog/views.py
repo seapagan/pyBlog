939a158113a45cfc19330307158c763232c19186
Grant Ramsay <seapagan@gmail.com>
2021-09-23 17:02:37 +0100

    Back to ListView for index again!
    TemplateView dows not support pagination.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,35 @@
 """Define the views for the 'blog' application."""
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView, DetailView
-from django.views.generic.base import TemplateView
+from django.views.generic import CreateView, DetailView, ListView
 
 from blog.models import Blog
 
-# class IndexClassView(ListView):
-#     """Define a TemplateView for the index (Blog main page)."""
 
-#     template_name = "blog/index.html"
-#     context_object_name = "blogs"
-#     paginate_by = 6
-#     model = Blog
-
-
-class IndexClassView(TemplateView):
+class IndexClassView(ListView):
     """Define a TemplateView for the index (Blog main page)."""
 
     template_name = "blog/index.html"
+    context_object_name = "blogs"
     paginate_by = 6
+    model = Blog
 
-    def get_context_data(self, **kwargs):
-        """Return the context for this view.
 
-        Doing it this way (and not using a ListView so as to enable multiple
-        models to be shown in the template (eg comment counts etc).
-        """
-        context = super(IndexClassView, self).get_context_data(**kwargs)
-        context["blogs"] = Blog.objects.all().order_by("-created_at")
+# class IndexClassView(TemplateView):
+#     """Define a TemplateView for the index (Blog main page)."""
 
-        return context
+#     template_name = "blog/index.html"
+#     paginate_by = 6
+
+#     def get_context_data(self, **kwargs):
+#         """Return the context for this view.
+
+#         Doing it this way (and not using a ListView so as to enable multiple
+#         models to be shown in the template (eg comment counts etc).
+#         """
+#         context = super(IndexClassView, self).get_context_data(**kwargs)
+#         context["blogs"] = Blog.objects.all().order_by("-created_at")
+
+#         return context
 
 
 class PostDetailView(DetailView):

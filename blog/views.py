23b6ad29ad72ed7ea6585cc19792b22606b65e95
Grant Ramsay <seapagan@gmail.com>
2021-09-28 14:19:18 +0100

    View to add a comment
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 """Define the views for the 'blog' application."""
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView
 
-from blog.models import Blog
+from blog.forms import CommentForm
+from blog.models import Blog, Comment
 
 
 class IndexClassView(ListView):
@@ -49,6 +51,43 @@ class NewPostView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         """Validate the form."""
         form.instance.user = self.request.user
-        print(self.request.user)
 
         return super().form_valid(form)
+
+
+class AddCommentView(CreateView):
+    """Add a new comment to a specific post."""
+
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_newcomment.html"
+    # fields = "__all__"
+
+    def get_context_data(self, **kwargs):
+        """Add extra context to the View.
+
+        This allows us to access Post-related stuff like the title from inside
+        our view.
+        """
+        context = super().get_context_data(**kwargs)
+        slug = self.kwargs["slug"]
+        context["post"] = Blog.objects.get(slug=slug)
+        context["form"].related_post = context["post"]
+        print("Context:", context)
+        return context
+
+    def form_valid(self, form):
+        """Validate the form."""
+        form.instance.related_post_id = Blog.objects.get(
+            slug=self.kwargs["slug"]
+        ).id
+        if self.request.user:
+            form.instance.created_by_user_id = self.request.user.id
+        # else:
+        #     form.instance.created_by_guest = "Guest User"
+
+        return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        """On success, return to the blog post we commented on."""
+        return reverse_lazy("blog:detail", kwargs={"slug": self.kwargs["slug"]})

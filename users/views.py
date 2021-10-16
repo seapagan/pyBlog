d990556f9264cf96f5c7921a583ce7d9cf548415
Grant Ramsay <seapagan@gmail.com>
2021-10-16 13:45:34 +0100

    add social buttons to profile
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 """Define views for the User App."""
+
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
@@ -6,6 +7,7 @@ from django.shortcuts import redirect, render
 from django.views.generic import DetailView, ListView
 
 from users.forms import RegisterForm
+from users.models import Profile
 
 
 # Create your views here.
@@ -43,3 +45,71 @@ class UserProfileView(DetailView):
     model = User
     template_name = "users/user-profile.html"
     context_object_name = "person"
+
+    def get_context_data(self, **kwargs):
+        """Add tags to this context, so we can use in the sidebar."""
+        context = super(UserProfileView, self).get_context_data(**kwargs)
+        # my_profile = Profile.objects.filter(user=self.object).values()[0]
+
+        context["links"] = self.get_profile_links()
+        # print(context)
+        return context
+
+    def get_profile_links(self):
+        """Take the profile links and return a fuller dictionary."""
+        my_profile = Profile.objects.filter(user=self.object).values()[0]
+        # remove certain fields that we dont want
+        unwanted = ["id", "user_id", "user", "image", "location"]
+        my_copy = my_profile.copy()
+        for item in my_copy:
+            if item in unwanted:
+                my_profile.pop(item)
+
+        # lookup dictionary
+        final = {
+            "website": {
+                "icon": "fal fa-globe",
+                "base_url": "",
+                "title": "Homepage",
+                "color": "var(--sidebar-links)",
+            },
+            "twitter_user": {
+                "icon": "fab fa-twitter-square",
+                "base_url": "https://twitter.com/",
+                "title": "Twitter",
+                "color": "#1DA1F2",
+            },
+            "github_user": {
+                "icon": "fab fa-github-square",
+                "base_url": "https://github.com/",
+                "title": "GitHub Page",
+                "color": "#333",
+            },
+            "youtube_user": {
+                "icon": "fab fa-youtube-square",
+                "base_url": "https://youtube.com/user/",
+                "title": "Youtube Channel",
+                "color": "#FF0000",
+            },
+            "linkedin_user": {
+                "icon": "fab fa-linkedin",
+                "base_url": "https://www.linkedin.com/in/",
+                "title": "LinkedIn",
+                "color": "#0077b5",
+            },
+            "facebook_user": {
+                "icon": "fab fa-facebook-square",
+                "base_url": "https://www.facebook.com/",
+                "title": "Facebook",
+                "color": "#4267B2",
+            },
+        }
+
+        # create empty final dictionary.
+
+        for item, value in my_profile.items():
+            final[item]["value"] = value
+
+        print(final)
+
+        return final

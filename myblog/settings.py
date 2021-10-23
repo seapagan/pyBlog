aca85ebe4ff491da0eeec17a254a3e47d1e079a6
Grant Ramsay <seapagan@gmail.com>
2021-10-23 16:04:37 +0100

    Basic styling of image upload widget
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- myblog/settings.py
+++ myblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "preferences",
+    "django.forms",
     "django_gravatar",
     "mdeditor",
     "fontawesomepro",

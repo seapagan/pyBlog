28964dd4f8f8ed997339eb7a875293f03cf84882
Grant Ramsay <seapagan@gmail.com>
2021-10-27 11:04:34 +0100

    add custom tags to return Post and Tag details
    These will be called in templates where needed, to cut down on the
    amount of global Context we were using
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,9 @@
 """Custom tag to remove draft posts in a template."""
 from django import template
 from django.db.models import Q
+from django.db.models.functions import Lower
+
+from blog.models import Blog, Tag
 
 register = template.Library()
 
@@ -16,3 +19,19 @@ def no_draft(tag_posts, user=None):
     except TypeError:
         filtered = tag_posts.exclude(draft=True)
     return filtered
+
+
+# the below 2 tags are used in the sidebar to pass extra context that is
+# required to properly display Latest Post and Tags.
+@register.simple_tag
+def sidebar_posts():
+    """Return first 5 posts in the database."""
+    return Blog.objects.all().order_by("-created_at")[:5]
+
+
+@register.simple_tag
+def sidebar_tags():
+    """Return all Tags in the database."""
+    # will later most likely restrict this to the top 20 or so tags sorted by
+    # number of related posts.
+    return Tag.objects.all().order_by(Lower("tag_name"))

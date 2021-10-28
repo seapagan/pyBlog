16f10e691523f738be17cb6d281e7c58341b9521
Grant Ramsay <seapagan@gmail.com>
2021-10-28 16:50:22 +0100

    FIX - custom error views were returning status 200
    Fixed to return 403/404 as required.
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- myblog/errors/views.py
+++ myblog/errors/views.py
@@ -21,10 +21,11 @@ def maintenance_mode(request, template_name="503.html"):
 def custom403(request, exception=None):
     """Create a custom 403 view, so we can add the page title context."""
     context = {"page_title": "Permission Denied!"}
-    return render(request, "403.html", context)
+    return render(request, "403.html", context, status=403)
+    return
 
 
 def custom404(request, exception=None):
     """Create a custom 404 view, so we can add the page title context."""
     context = {"page_title": "Not Found"}
-    return render(request, "404.html", context)
+    return render(request, "404.html", context, status=404)

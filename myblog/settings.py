1d9d5fd5274a0232568312cec7dcedf8e34750d6
Grant Ramsay <seapagan@gmail.com>
2021-12-11 16:57:48 +0000

    'humanize' the comment posting times
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- myblog/settings.py
+++ myblog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.humanize",
     "preferences",
     "django.forms",
     "django_gravatar",

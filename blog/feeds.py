888a10c0b109eb7ef5c0ad621509f942f2941415
Grant Ramsay <seapagan@gmail.com>
2021-12-12 10:59:18 +0000

    add further keys to the RSS feed.
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/feeds.py
+++ blog/feeds.py
@@ -1,7 +1,7 @@
 """Configure RSS feeds for the Blog model."""
 from django.contrib.syndication.views import Feed
 
-from blog.models import Blog
+from blog.models import Blog, Tag
 
 
 class PostsFeed(Feed):
@@ -10,6 +10,7 @@ class PostsFeed(Feed):
     title = "Tek:Cited Posts"
     link = "/"
     description = "Updates from Tek:Cited."
+    feed_copyright = "Copyright (c) 2021, Seapagan"
 
     def items(self):
         """Return the latest blog posts."""
@@ -22,3 +23,20 @@ class PostsFeed(Feed):
     def item_description(self, item):
         """Return the description of the blog post."""
         return item.desc
+
+    def item_author_name(self, item):
+        """Returns the author's name."""
+        return item.user.username.capitalize()
+
+    def item_pubdate(self, item):
+        """Returns the post's published date."""
+        return item.created_at
+
+    def item_updateddate(self, item):
+        """Returns the post's updated date."""
+        return item.updated_at
+
+    def item_categories(self, item):
+        """Returns the tags for this post as categories."""
+        tags = Tag.objects.filter(posts=item)
+        return list(tags.values_list("tag_name", flat=True))

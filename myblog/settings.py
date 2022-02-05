3a72907efac2e2c28c6b077805312b57b02793d9
Grant Ramsay <seapagan@gmail.com>
2022-02-05 13:03:32 +0000

    tidy up settings.py
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- myblog/settings.py
+++ myblog/settings.py
@@ -19,14 +19,10 @@ load_dotenv(override=True)  # loads the configs from .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+# Read the SECRTE_KEY from the .env file
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# Set DEBUG from the .env file
 DEBUG = bool(int(os.getenv("DEBUG", 0)))
 
 # Add extra hosts from the .env file if specified
@@ -320,4 +316,4 @@ RECAPTCHA_PRIVATE_KEY = str(os.getenv("RECAPTCHA_PRIVATE_KEY"))
 
 # rundevserver settings
 RDS_ALL_INTERFACES = True
-# RDS_DEBUG = False
+RDS_DEBUG = False

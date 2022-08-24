89050f5db837b5b2aa63a1ec7a860c07122b2778
Grant Ramsay <seapagan@gmail.com>
2022-08-24 11:46:08 +0100

    add a template tag to return the Analytics key
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,6 @@
 """Several custom Template tags to make things easier."""
+import os
+
 from django import template
 from django.db.models import Q
 from django.db.models.functions import Lower
@@ -27,6 +29,12 @@ def by_hits(posts):
     return posts.order_by(
         "-hit_count_generic__hits", "-total_upvotes", "-created_at"
     )
+
+
+@register.simple_tag
+def analytics():
+    """Return the analytics tag from the environment."""
+    return str(os.getenv("GOOGLE_ANALYTICS"))
 
 
 # the below tag is used in the sidebar to pass extra context that is needed to
